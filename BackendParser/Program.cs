8fe6a4d966d70a907964cd424c1b827d57799871
sharif ahmad <sharifahmad2061@gmail.com>
2020-09-15 13:27:20 +0500

    Initial Commit

---
+++ BackendParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackendParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MessageHandler/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessageHandler
+{
+    public class TaskModel
+    {
+        public string PracticeCode { get; set; }
+        public string DataStorePath { get; set; }
+        public bool EmailOnCompletion { get; set; }
+        public string RecepientEmail { get; set; }
+    }
+}
