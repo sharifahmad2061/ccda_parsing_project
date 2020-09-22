83b30fa7f3be17b808bf304944460cdac9fbbeb0
sharif ahmad <sharifahmad2061@gmail.com>
2020-09-22 10:26:48 +0500

    created a basic implementation for the QueueTask

--- MessageHandler/Services/CreateMessage.cs
+++ MessageHandler/Services/CreateMessage.cs
@@ -1,12 +1,28 @@
+using System;
 using MessageHandler;
+using Microsoft.Extensions.Configuration;
 
 namespace Services
 {
     public class CreateMessage : ICreateMessage
     {
+        private readonly string _host;
+        private readonly string _port;
+        private readonly string _exchange;
+        private readonly string _queue;
+        public CreateMessage(string host, string port, string exchange, string queue)
+        {
+            _host = host;
+            _port = port;
+            _exchange = exchange;
+            _queue = queue;
+        }
         public void QueueTask(TaskModel model)
         {
-            throw new System.NotImplementedException();
+            string rmqHost = _host;
+            string port = _port;
+            Console.WriteLine(rmqHost);
+            Console.WriteLine(port);
         }
     }
 }
