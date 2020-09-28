91f27ed2fea8e89124b229d2a705ce9a91644c05
sharif ahmad <sharifahmad2061@gmail.com>
2020-09-28 14:35:23 +0500

    coded the add AddRabbitMqClient service and its configuration

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -25,7 +25,7 @@ namespace Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddMessageHandler();
+            services.AddMessageHandler(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- MessageHandler/IServiceCollectionExtension.cs
+++ MessageHandler/IServiceCollectionExtension.cs
@@ -1,13 +1,18 @@
 using Microsoft.Extensions.DependencyInjection;
 using MessageHandler.Services;
+using RabbitMQ.Client.Core.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+// using RabbitMq.Client;
 
 namespace MessageHandler
 {
     public static class IServiceCollectionExtension
     {
-        public static IServiceCollection AddMessageHandler(this IServiceCollection services)
+        public static IServiceCollection AddMessageHandler(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<ICreateMessage, CreateMessage>();
+            services.AddRabbitMqClient(configuration.GetSection("rabbitmq"));
+            // services.AddRabbitMqClient();
             return services;
         }
     }
